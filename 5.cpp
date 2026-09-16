a408ea4c37dd1a5b92da3bccf8df7477c4c72963
Aditya_Kumar <aditya.224.kr@gmail.com>
2026-09-16 20:43:35 +0530

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+cout<<"following are the types of operators in c++"<<endl;
+int a=4 , b=5;
+cout<<"the value of a+b is :"<<a+b<<endl;
+cout<<"the value of a-b is :"<<a-b<<endl;
+cout<<"the value of a*b is :"<<a*b<<endl;
+cout<<"the value of a%b is :"<<a%b<<endl;
+cout<<"the value of a/b is :"<<a/b<<endl;
+cout<<"the value of a++ is :"<<a++<<endl;
+cout<<"the value of a-- is :"<<a--<<endl;
+cout<<"the value of ++a is :"<<++a<<endl;
+cout<<"the value of --a is :"<<--a<<endl;
+return 0;
+}
