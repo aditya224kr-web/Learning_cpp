9cdf66bb6381715eecc789634261f311e6b8c81a
Aditya_Kumar <aditya.224.kr@gmail.com>
2026-09-18 12:07:30 +0530

    Add files via upload

---
+++ 7.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a = 4, b= 5;
+cout<<"following are the logical operators"<<endl;
+cout<<" the value of and operator ((a==b) && (a<b)) is :"<<((a==b) && (a<b))<<endl;
+cout<<" the value of or operator ((a==b) || (a<b)) is :"<<((a==b) || (a<b))<<endl;
+cout<<" the value of not operator (!(a==b)) is :"<<(!(a==b))<<endl;
+return 0;
+}
